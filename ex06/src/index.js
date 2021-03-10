ba38995b1fc0bd5465fff9a1964c3bf6e3e0df74
mstfhlc <mustafahalici30@gmail.com>
2021-03-10 19:09:26 +0100

    creating ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,24 @@
+// Only change below this line
+var x;
+var y;
+var z;
+
+// Only change below this line
+
+x = x + 9;
+y = y + 16;
+z = z + "My name is";
+
+function main() {
+    // Only change below this line
+
+    // Only change below this line
+    x = x + 2;
+    y = y + 4;
+    z = z + "My name is John";
+    return [x,y,z];
+
+}
+
+console.log(main());
+module.exports=main;
