6e7de1f1872ef136d9c8ae68a02695592a9173da
mstfhlc <mustafahalici30@gmail.com>
2021-03-10 16:03:05 +0100

    creating ex02

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,5 +1,5 @@
 function main(){
-     //only change code below this line
+      //only change code below this line
       //only change code below this line
 console.log("Hello Word!")
 
---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+// this is a single line JavaScript comment
+// this is a single line JavaScript comment
+// this is a single line JavaScript comment
+// this is a single line JavaScript comment
+// this is a single line JavaScript comment
+
+/*document.write("This is a multi-line comment");
+document.write("This is a multi-line comment");
+document.write("This is a multi-line comment");
+document.write("This is a multi-line comment");
+document.write("This is a multi-line comment");
+*/
