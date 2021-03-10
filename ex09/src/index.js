02b7b8bd8b2109226965d437f9722e94365ef80c
mstfhlc <mustafahalici30@gmail.com>
2021-03-10 23:18:03 +0100

    creating ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,18 @@
+function main (numOne,numTwo){
+    // Only change code below this line
+
+   
+    sum=numOne-numTwo;
+
+
+    // Only change code below this line
+    return sum;
+}
+
+//console.log(main(numOne,numTwo));  // change this line
+console.log(main(15,8));
+console.log(main(25,8));
+console.log(main(425,120));
+
+
+module.exports=main;
