db15b4296c272d03783f13e95a4dae34c0d6a348
mstfhlc <mustafahalici30@gmail.com>
2021-03-10 16:24:35 +0100

    creating ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    // only change code below this line
+    x=10;
+    y=10;
+    x=y;
+    // only change code below this line
+ return[x,y];
+}
+
+console.log(main());
+module.expots=main;
